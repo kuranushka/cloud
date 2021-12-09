3b352a5428647b119fef6b19eea296250085ed52
Andrey Kuranov <kuranov.andrey@gmail.com>
2021-12-09 17:07:23 +0300

    Send command worked

--- src/main/java/ru/kuranov/client/handler/Command.java
+++ src/main/java/ru/kuranov/client/handler/Command.java
@@ -3,29 +3,11 @@ package ru.kuranov.client.handler;
 import java.util.Arrays;
 
 public enum Command {
-    LEVEL_UP(".."),
-    OPEN("open"),
-    CREATE("touch"),
-    SEND("send"),
-    RECEIVE("receive"),
-    RENAME("rename"),
-    DELETE("delete");
-
-    private final String command;
-
-
-    Command(String command) {
-        this.command = command;
-    }
-
-    public static Command byCommand(String command) {
-        return Arrays.stream(values())
-                .filter(cmd -> cmd.getCommand().equals(command))
-                .findAny()
-                .orElseThrow(() -> new RuntimeException("Command not found"));
-    }
-
-    public String getCommand() {
-        return command;
-    }
+    LEVEL_UP,
+    OPEN,
+    CREATE,
+    SEND,
+    RECEIVE,
+    RENAME,
+    DELETE;
 }
